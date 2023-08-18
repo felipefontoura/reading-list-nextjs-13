467bb2938d3151783035cc85bed63e203f74a52f
Felipe Fontoura <ffontouras@gmail.com>
2023-08-18 17:02:53 -0300

    removed unused sections

--- src/app/components/Goals.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Goals() {
-  return (
-    <section className="mb-8">
-      <h2>Metas</h2>
-    </section>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import NavBar from "./components/NavBar";
 import Hero from "@components/Hero";
 import MyBooks from "@/app/components/MyBooks";
-import Goals from "@components/Goals";
 import Feed from "@/app/components/Feed";
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
           <div className="md:basis-2/3">
             <Hero />
             <MyBooks />
-            <Goals />
           </div>
           <div className="md:basis-1/3">
             <Feed />
